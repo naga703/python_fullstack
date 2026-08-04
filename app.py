799ddc7d14b6c9960a940c9e5310ed1e2417624e
naga703 <chillapalliswapna@gmail.com>
2026-08-04 10:49:11 +0000

    updated code

--- app.py
+++ app.py
@@ -19,5 +19,16 @@ def courses():
 def trainers():
     return render_template("trainers.html")
 
+@app.route("/register")
+def register():
+    if request.method=="POST":
+        name=request.form["name"]
+        email=request.form["email"]
+        password=request.form["password"]
+        dob=request.form["dob"]
+        gender=request.form["gender"]
+        course=request.form["course"]
+        return render_template("register.html")     
+
 if __name__ =='__main__':
     app.run(debug=True)
