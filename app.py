ed97bd175d36c47e1bd8e4c1ff8561c5bef60d19
naga703 <chillapalliswapna@gmail.com>
2026-08-04 09:29:13 +0000

    folder allocation

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from flask import Flask,render_template
 
-app = 
+app = Flask(__name__)
 Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, World!"
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/courses")
+def courses():
+    return render_template("courses.html")
+
+@app.route("/trainers")
+def trainers():
+    return render_template("trainers.html")
 
 if __name__ =='__main__':
     app.run(debug=True)
